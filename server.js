235193917e8113ebb0064bcf55cda69a825e8126
[R3dact3d] <emmettwb@gmail.com>
2018-03-26 16:42:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
 
+if (process.argv[2] == undefined) {
+  var port = 80;
+} else {
+  var port = process.argv[2]; // Use node server.js <port>
+}
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(port, function(){
+  console.log('listening on *:'+port);
+});
