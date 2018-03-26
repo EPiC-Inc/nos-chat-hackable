178d64ee36a2438f803521a58f6c953573b7d431
[R3dact3d] <emmettwb@gmail.com>
2018-03-26 17:10:37 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var url = require('url'); 
 
 if (process.argv[2] == undefined) {
   var port = 80;
@@ -14,6 +15,10 @@ res.sendFile(__dirname + '/static/index.html');
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  console.log(socket.handshake.url);
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(port, function(){
