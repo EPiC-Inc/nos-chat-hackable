d40f6c1ce737636d8abbb285c2e8b08c95d4fe8b
[R3dact3d] <emmettwb@gmail.com>
2018-03-26 17:31:46 -0600

    Create chat.js

---
+++ static/chat.js
@@ -0,0 +1,10 @@
+var socket = io();
+console.log(window.location.search);
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
