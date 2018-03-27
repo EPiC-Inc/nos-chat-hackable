150beb1b843fc58ba81a629c2f92178910c9e5da
[R3dact3d] <emmettwb@gmail.com>
2018-03-26 18:09:57 -0600

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -1,10 +1,11 @@
 var socket = io();
-console.log(window.location.search);
 
 function getUrlVars() {
     var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    var parts = window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
         vars[key] = value;
     });
     return vars;
 }
+
+console.log(getUrlVars()['room']);
