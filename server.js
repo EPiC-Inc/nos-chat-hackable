9d4ecea62f07ac4aa7147c03fed03ce83039c7a1
[R3dact3d] <emmettwb@gmail.com>
2018-03-27 09:56:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
-    io.to(users[socket.id][1]).emit('User ['+users[socket.id][0]+'] has left');
+    io.to(users[socket.id][1]).emit('User  has left');
     delete users[socket.id];
   });
   socket.on('switch', function(data){
     console.log('user switched');
     socket.join(data[0]);
-    io.to(data[0]).emit('message', 'User ['+data[1]+'] has joined')
+    io.to(data[0]).emit('message', 'User  has joined')
     users[socket.id] = [data[1], data[0]];
     console.log(users);
   });
