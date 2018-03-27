4dd2a0e04761a5cbbf652eb3e283e3a11571cbbb
[R3dact3d] <emmettwb@gmail.com>
2018-03-26 19:09:04 -0600

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -1,11 +1,25 @@
+// Vars
 var socket = io();
+var key = document.getElementById("passkey");
+var keyPage = document.getElementById("keyPage");
+var passwd = 'ocelot';
 
+// Functions
 function getUrlVars() {
     var vars = {};
     var parts = window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
         vars[key] = value;
     });
     return vars;
+}
+
+// Callbacks
+key.oninput = function(event) {
+    if (key.value == passwd) {
+        key.value = '';
+        key.disabled = true;
+        keyPage.style.visibility = 'hidden';
+    }
 }
 
 room = getUrlVars()['room']
