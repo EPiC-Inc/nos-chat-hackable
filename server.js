1344958eb4603b138c395973ae64d3e7db059d00
[R3dact3d] <emmettwb@gmail.com>
2018-03-26 18:47:50 -0600

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.argv[2] == undefined) {
 // Routing
 app.use('/static', express.static(__dirname + '/static'));
 app.get('/', function(req, res){
-res.sendFile(__dirname + '/static/index.html');
+  res.sendFile(__dirname + '/static/index.html');
 });
 
 io.on('connection', function(socket){
