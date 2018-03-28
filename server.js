84b235491bb4307b41e249d458dd3ea15102292e
[R3dact3d] <emmettwb@gmail.com>
2018-03-28 13:42:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -38,21 +38,23 @@ io.on('connection', function(socket){
   socket.on('message', function(data) {
     var msg = data['data'][1];
     io.to(data['room']).emit('message', data['data']);
-    if (msg == '?ping') {
-      for (usr in users) {
-        io.to(data['room']).emit('message', ['_System', users[usr]]);
-      }
-    } else if (msg == '?ping room') {
-      for (usr in users) {
-        if (users[usr][1] == data['room']) {
+    if (msg.startsWith('?') {
+      if (msg == '?ping') {
+        for (usr in users) {
           io.to(data['room']).emit('message', ['_System', users[usr]]);
         }
+      } else if (msg == '?ping room') {
+        for (usr in users) {
+          if (users[usr][1] == data['room']) {
+            io.to(data['room']).emit('message', ['_System', users[usr]]);
+          }
+        }
+      } else if (msg == '?help') {
+        io.to(data['room']).emit('message', ['_System', cmdHelp]);
+      } else {
       }
-    } else if (msg == '?help') {
-      io.to(data['room']).emit('message', ['_System', cmdHelp]);
-    } else {
+      //console.log('data');
     }
-    //console.log('data');
   });
 });
 
