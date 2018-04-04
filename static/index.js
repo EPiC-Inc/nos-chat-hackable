d95030b6b870562d39fa08c0a66811d53fbfc8bb
[R3dact3d] <emmettwb@gmail.com>
2018-04-03 18:00:21 -0600

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,11 +1,5 @@
 var room = document.getElementById("roomName"); // Room ID / Name
 
-function join() {window.location.href = "/static/coms.html?room="room.value;}
-
-// Jquery
-$(function () {
-    $('form').submit(function(){
-        sendMsg();
-        return false;
-    });
-});
+function join() {
+    window.location.href = "/static/coms.html?room="room.value;
+}
